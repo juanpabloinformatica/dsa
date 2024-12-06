669c6011f77502b399dd68eb74bf06b2ee711794
Juan Pablo Prada Mejia <pradamej@ensipc71.ensimag.fr>
2024-12-06 11:00:23 +0100

    Missing the test for bst

--- test/testBst.cpp
+++ test/testBst.cpp
@@ -28,7 +28,34 @@ TEST(BstTest, HandleAddElement) {
   int *ptrTempValue = &tempValue;
   BstNode *bstNode = newBstNode(ptrTempValue);
   bst->addBstNode(bst, bstNode);
-  ASSERT_TRUE(*(int *)bst->root == tempValue);
+  /* std::cout<< *(int*)bst->root->value <<std::endl; */
+  /* ASSERT_TRUE(*(int *)bst->root->value == tempValue); */
+  /* // for more than one value */
+  /* const int ARRAY_LENGTH = 6; */
+  /* int elements[ARRAY_LENGTH]; */
+  /* for (int i = 0; i < ARRAY_LENGTH; i++) { */
+  /*   elements[i] = i; */
+  /*   BstNode* bstNode = newBstNode((int*)&elements[i]); */
+  /*   bst->addBstNode(bst,bstNode); */
+  /* } */
+  // test for check is working well
+  // root = 10
+  // [20,5,9,25,6]
+  // root->right = 20 | root->left=5 | root->left->right=9 | root->right->right=25 | root->left->right->right = 6
+  //
+  int testArrayLength = 5;
+  int testArray[testArrayLength] = {20,5,9,25,6};
+  for(int i = 0;i<testArrayLength;i++){
+      testArray[i] = i;
+      BstNode* bstNode = newBstNode((int*)&testArray[i]);
+      bst->addBstNode(bst,bstNode);
+  }
+  ASSERT_TRUE(*(int*)bst->root->value==10);
+  /* ASSERT_TRUE(*(int*)bst->root->right->value == 20); */
+  /* ASSERT_TRUE(*(int*)bst->root->left->value == 5); */
+  /* ASSERT_TRUE(*(int*)bst->root->left->right->value == 9); */
+  /* ASSERT_TRUE(*(int*)bst->root->right->right->value == 25); */
+  /* ASSERT_TRUE(*(int*)bst->root->left->right->right->value == 25); */
 }
 // TEST(BstTest,HandleRemoveElement){
 //

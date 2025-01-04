47f48aabf47bf3914604e8decb99055f00a4850e
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2025-01-04 11:27:31 +0100

    Letting hashmap local branch to be in charge of refactoring the hashmap. So getting back in this branch to older implementation.

--- implementation/hashmap.h
+++ implementation/hashmap.h
@@ -1,8 +1,6 @@
 #ifndef __HASH_MAP__
 #define __HASH_MAP__
-#include "dynamicArray.h"
 #include "linkedList.h"
-#include "graph.h"
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -14,12 +12,6 @@
 #define MODULE_FACTOR ARRAY_MAX_SIZE
 #define HASHMAP_TYPE_NUMBER "number"
 #define HASHMAP_TYPE_STRING "string"
-#define HASHMAP_TYPE_USER_DEFINED "void*"
-#define HASHFUNCTION(hashmap, key)                                             \
-  _Generic((key),                                                              \
-      int: hashFunctionNumber((hashmap), (key)),                               \
-      char *: hashFunctionString((hashmap), (key)),                            \
-      void *: hashFunctionUserDefined((hashmap), (key)))
 
 #define IS_NUMBER(variable) (())
 typedef struct HashmapNode HashmapNode;
@@ -32,12 +24,11 @@ typedef struct Hashmap Hashmap;
 struct Hashmap {
   char *keyType;
   char *valueType;
-  // void *array[ARRAY_MAX_SIZE];
-  DynamicArray *array;
+  void *array[ARRAY_MAX_SIZE];
   int counter;
   void (*hashmapPut)(Hashmap *hashmap, void *key, void *value);
   // void *(*hashmapGet)(Hashmap *hashmap, void *key);
-  void *(*hashmapGet)(Hashmap *hashmap, void *key);
+  void* (*hashmapGet)(Hashmap *hashmap, void *key);
   void (*hashmapRemove)(Hashmap *hashmap, void *key);
   bool (*hashmapContainsKey)(Hashmap *hashmap, void *key);
   // bool (*hashmapContainsValue)(Hashmap *hashmap, void *value);
@@ -48,13 +39,10 @@ Hashmap *newHashmap(char *keyType, char *valueType);
 int hashFunction(Hashmap *hashmap, void *key);
 void hashmapPut(Hashmap *hashmap, void *key, void *value);
 // void* hashmapGet(Hashmap *hashmap, void *key);
-void *hashmapGet(Hashmap *hashmap, void *key);
+void* hashmapGet(Hashmap *hashmap, void *key);
 void hashmapRemove(Hashmap *hashmap, void *key);
 bool hashmapContainsKey(Hashmap *hashmap, void *key);
 // bool hashmapContainsValue(Hashmap *hashmap, void *value);
 void destroyHashmap(Hashmap *hashmap);
-int hashFunctionNumber(Hashmap *hashmap, void *key);
-int hashFunctionString(Hashmap *hashmap, void *key);
-int hashFunctionUserDefined(Hashmap *hashmap, void *key);
 // void destroyHashmapNode(HashmapNode *hashmapNode);
 #endif

de464a6f1535ac8c07167a0b89b22065141ec58b
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2025-01-04 11:12:51 +0100

    Changing hashmap implementation for more generic approach.

--- implementation/hashmap.c
+++ implementation/hashmap.c
@@ -8,7 +8,7 @@ Hashmap *newHashmap(char *keyType, char *valueType) {
   Hashmap *ptrHashmap = (Hashmap *)malloc(sizeof(Hashmap));
   ptrHashmap->keyType = (char *)malloc(sizeof(char) * strlen(keyType));
   ptrHashmap->valueType = (char *)malloc(sizeof(char) * strlen(valueType));
-  ptrHashmap->counter = 0;
+  ptrHashmap->array = newDynamicArray();
   memcpy(ptrHashmap->keyType, keyType, strlen(keyType));
   memcpy(ptrHashmap->valueType, valueType, strlen(valueType));
   ptrHashmap->hashmapPut = hashmapPut;
@@ -25,39 +25,54 @@ static int _stringGetSum(int sum, char *key) {
   _stringGetSum(sum, key + 1);
   return sum;
 }
-int hashFunction(Hashmap *hashmap, void *key) {
-  assert(hashmap != NULL);
-  assert(hashmap->hashmapContainsKey(hashmap, key) == true);
-  assert(strcmp(hashmap->keyType, HASHMAP_TYPE_NUMBER) == 0 ||
-         strcmp(hashmap->keyType, HASHMAP_TYPE_STRING) == 0);
-  if (strcmp(hashmap->keyType, HASHMAP_TYPE_STRING) == 0) {
-    int sum = _stringGetSum(0, key);
-    return sum % MODULE_FACTOR;
-  } else {
-    int keyNumber = *(int *)key;
-    return keyNumber % MODULE_FACTOR;
-  }
+int hashFunctionString(Hashmap *hashmap, void *key) {
+  int sum = _stringGetSum(0, key);
+  return sum % hashmap->array->size;
 }
+int hashFunctionNumber(Hashmap *hashmap, void *key) {
+  int keyNumber = *(int *)key;
+  return keyNumber % hashmap->array->size;
+}
+// if hashmap grows i need to reorganize
+int hashFunctionUserDefined(Hashmap *hashmap, void *key) {
+  // case is a vertex type of a number
+  int sum = *(int *)(((Vertex *)key)->value);
+  return sum % hashmap->array->size;
+}
+/* int hashFunction(Hashmap *hashmap, void *key) { */
+/*   assert(hashmap != NULL); */
+/*   assert(hashmap->hashmapContainsKey(hashmap, key) == true); */
+/*   assert(strcmp(hashmap->keyType, HASHMAP_TYPE_NUMBER) == 0 || */
+/*          strcmp(hashmap->keyType, HASHMAP_TYPE_STRING) == 0); */
+/*   if (strcmp(hashmap->keyType, HASHMAP_TYPE_STRING) == 0) { */
+/*     int sum = _stringGetSum(0, key); */
+/*     return sum % MODULE_FACTOR; */
+/*   } else { */
+/*     int keyNumber = *(int *)key; */
+/*     return keyNumber % MODULE_FACTOR; */
+/*   } */
+/* } */
 void hashmapPut(Hashmap *hashmap, void *key, void *value) {
   assert(hashmap != NULL);
   assert(key != NULL);
   assert(value != NULL);
   assert(strcmp(hashmap->keyType, HASHMAP_TYPE_NUMBER) == 0 ||
          strcmp(hashmap->keyType, HASHMAP_TYPE_STRING) == 0);
-  int getIndex = hashFunction(hashmap, key);
-  if (hashmap->array[getIndex] != NULL) {
-    LinkedList *list = hashmap->array[getIndex];
-    HashmapNode *hashmapNode = newHashmapNode(key, value);
-    LinkedListNode *node = newLinkedListNode(hashmapNode);
-    list->addLinkedList(list, node);
-    list->showLinkedListNode(list);
-    return;
-  }
-  LinkedList *list = newLinkedList();
-  HashmapNode *hashmapNode = newHashmapNode(key, value);
-  LinkedListNode *node = newLinkedListNode(hashmapNode);
-  list->addLinkedList(list, node);
-  hashmap->array[getIndex] = list;
+  /* int getIndex = hashFunction(hashmap, key); */
+  int getIndex = HASHFUNCTION(hashmap, key);
+  /* if (hashmap->array[getIndex] != NULL) { */
+  /*   LinkedList *list = hashmap->array[getIndex]; */
+  /*   HashmapNode *hashmapNode = newHashmapNode(key, value); */
+  /*   LinkedListNode *node = newLinkedListNode(hashmapNode); */
+  /*   list->addLinkedList(list, node); */
+  /*   list->showLinkedListNode(list); */
+  /*   return; */
+  /* } */
+  /* LinkedList *list = newLinkedList(); */
+  /* HashmapNode *hashmapNode = newHashmapNode(key, value); */
+  /* LinkedListNode *node = newLinkedListNode(hashmapNode); */
+  /* list->addLinkedList(list, node); */
+  /* hashmap->array[getIndex] = list; */
 }
 void *hashmapGet(Hashmap *hashmap, void *key) {
   assert(hashmap->hashmapContainsKey(hashmap, key) == true);
--- implementation/hashmap.h
+++ implementation/hashmap.h
@@ -1,6 +1,8 @@
 #ifndef __HASH_MAP__
 #define __HASH_MAP__
+#include "dynamicArray.h"
 #include "linkedList.h"
+#include "graph.h"
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -12,6 +14,12 @@
 #define MODULE_FACTOR ARRAY_MAX_SIZE
 #define HASHMAP_TYPE_NUMBER "number"
 #define HASHMAP_TYPE_STRING "string"
+#define HASHMAP_TYPE_USER_DEFINED "void*"
+#define HASHFUNCTION(hashmap, key)                                             \
+  _Generic((key),                                                              \
+      int: hashFunctionNumber((hashmap), (key)),                               \
+      char *: hashFunctionString((hashmap), (key)),                            \
+      void *: hashFunctionUserDefined((hashmap), (key)))
 
 #define IS_NUMBER(variable) (())
 typedef struct HashmapNode HashmapNode;
@@ -24,11 +32,12 @@ typedef struct Hashmap Hashmap;
 struct Hashmap {
   char *keyType;
   char *valueType;
-  void *array[ARRAY_MAX_SIZE];
+  // void *array[ARRAY_MAX_SIZE];
+  DynamicArray *array;
   int counter;
   void (*hashmapPut)(Hashmap *hashmap, void *key, void *value);
   // void *(*hashmapGet)(Hashmap *hashmap, void *key);
-  void* (*hashmapGet)(Hashmap *hashmap, void *key);
+  void *(*hashmapGet)(Hashmap *hashmap, void *key);
   void (*hashmapRemove)(Hashmap *hashmap, void *key);
   bool (*hashmapContainsKey)(Hashmap *hashmap, void *key);
   // bool (*hashmapContainsValue)(Hashmap *hashmap, void *value);
@@ -39,10 +48,13 @@ Hashmap *newHashmap(char *keyType, char *valueType);
 int hashFunction(Hashmap *hashmap, void *key);
 void hashmapPut(Hashmap *hashmap, void *key, void *value);
 // void* hashmapGet(Hashmap *hashmap, void *key);
-void* hashmapGet(Hashmap *hashmap, void *key);
+void *hashmapGet(Hashmap *hashmap, void *key);
 void hashmapRemove(Hashmap *hashmap, void *key);
 bool hashmapContainsKey(Hashmap *hashmap, void *key);
 // bool hashmapContainsValue(Hashmap *hashmap, void *value);
 void destroyHashmap(Hashmap *hashmap);
+int hashFunctionNumber(Hashmap *hashmap, void *key);
+int hashFunctionString(Hashmap *hashmap, void *key);
+int hashFunctionUserDefined(Hashmap *hashmap, void *key);
 // void destroyHashmapNode(HashmapNode *hashmapNode);
 #endif
